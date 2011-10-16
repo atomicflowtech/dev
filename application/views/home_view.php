3dd0b55e74609f687c7db299294722038a2008e7
Lucas Paulger <lucas.paulger@gmail.com>
2011-10-16 15:59:43 -0400

    syncronized css and images to show as old site did

---
+++ application/views/home_view.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
+		Remove this if you use the .htaccess -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<title>AtomicFlow Creative Tech Dev Site</title>
+		<meta name="description" content="The official Development Site of AtomicFlow" />
+		<meta name="author" content="Brandon John-Freso and Lucas Paulger" />
+		<meta name="viewport" content="width=device-width; initial-scale=1.0" />
+		<link rel="stylesheet" type="text/css" href="css/fontfaces.css" media="all" />
+		<link rel="stylesheet" type="text/css" href="css/grid/reset.css" media="all" />
+		
+		
+		<link rel="stylesheet" type="text/css" href="css/styles.css" media="all" />
+		<link rel="stylesheet" type="text/css" href="css/grid/960.css" />
+	</head>
+	<body>
+			
+			<header>
+				<div id="headerTopArea"class="container_16">
+					<h2 id="headerTopAreaGraphic" class="grid_16">HTML CSS DESIGN MARKETING</h2>
+				</div> <!-- end headerTopArea -->
+				
+				<div id="headerBar">
+					<div id="headerBarContent" class="container_16">
+						<a href="http://dev.atomicflowtech.com"><h1 class="grid_5 suffix_1">AtomicFlow</h1></a>
+						<nav id="mainNavigation">
+							<ul id="mainNavigationList">
+								<li class="alpha"><a href="#">About</a></li>
+								<li><a href="#">Skills</a></li>
+								<li><a href="#">Work</a></li>
+								<li><a href="#">Contact</a></li>
+							</ul> <!-- end mainNavigationList -->
+						</nav> <!-- end mainNavigation -->
+					</div> <!-- end headerBarContent -->		
+				</div> <!-- end headerBar -->
+				<div id="headerBottomArea"class="container_16">
+					<img id="leftSpout" src="images/backgrounds/leftSpout.png" alt="" /><img id="rightSpout" src="images/backgrounds/rightSpout.png" alt="" />
+					<h2 id="headerBottomAreaGraphic" class="">Creative. Intelligent. Quick.</h2>
+				</div>
+				
+				
+			</header>
+			<section class="mainContent container_16">
+				<div id="wordFeaturette">
+					<p class="grid_16">
+						AtomicFlow is an independent <span class="blue1">digital</span> design firm based in Washington, DC. We work with clients to create <span class="pink">interactive experiences</span> via <span class="green1">websites</span> and <span class="orange1">applications</span> that engage customers. We are <span class="orange2">perfectionists</span> in every sense of the word, <span class="yellow">commited</span> to ensuring our <span class="red">dedication</span> shows in every <span class="blue2">detail</span> of our work. If you can <span class="purple">think</span> it, we can <span class="green2">build</span> it.
+					</p>
+				</div> <!-- end wordFeaturette -->
+				
+				<div id="sliderFeaturette"></div>
+			</section>
+			<footer>
+				
+			</footer>
+	</body>
+</html>
