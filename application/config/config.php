f75a2ea6aab1e929b362bafac5d380ae82d7db5b
Brandon John-Freso <brandonjf@atomicflowtech.com>
2011-10-23 20:22:18 -0400

    Changed the Base URL in the Config PHP to AtomicFlowDev

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/dev/';
+$config['base_url']	= 'http://localhost/AtomicFlowDev/';
 
 /*
 |--------------------------------------------------------------------------
