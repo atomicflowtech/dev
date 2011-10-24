a12bf87a6bead8d1d2bcbfc498a27e0c38aa101e
Lucas Paulger <lucas.paulger@gmail.com>
2011-10-24 18:11:19 -0400

    change template names back again

--- application/hooks/template.php
+++ application/hooks/template.php
@@ -5,17 +5,17 @@ class Template{
         $this->CI =& get_instance();
     }
 	
-	function header() {
+	function head() {
 		$this->CI->load->library('parser');
 		$data = array(
             'page_title' => 'AtomicFlow Creative Tech Dev Site',
             'page_description' => 'The official Development Site of AtomicFlow'
             );
-		$this->CI->parser->parse('template/header_view', $data);
+		$this->CI->parser->parse('template/head_view', $data);
 	}
 	
-	function nav() {
-		$this->CI->load->view('template/nav_view');
+	function header() {
+		$this->CI->load->view('template/header_view');
 	}
 	
 	function footer() {
--- application/views/template/header_view.php
+++ application/views/template/header_view.php
@@ -1,39 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8" />
-		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
-		Remove this if you use the .htaccess -->
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-		<title>{page_title}</title>
-		<meta name="description" content="{page_description}" />
-		<meta name="author" content="Brandon John-Freso and Lucas Paulger" />
-		<meta name="viewport" content="width=device-width; initial-scale=1.0" />
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/fontfaces.css" media="all" />
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/grid/reset.css" media="all" />
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/default.css" media="all" />
-		
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/styles.css" media="all" />
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/grid/960.css" />
-		<script type="text/javascript">
-		//Script Brandon added to override default styles in the 960 Gridder overlay
-var gOverride = {
-  urlBase: 'http://gridder.andreehansson.se/releases/latest/',
-  gColor: '#EEEEEE',
-  gColumns: 16,
-  gOpacity: 0.35,
-  gWidth: 10,
-  pColor: '#C0C0C0',
-  pHeight: 15,
-  pOffset: 0,
-  pOpacity: 0.55,
-  center: true,
-  gEnabled: true,
-  pEnabled: false,//modified from true
-  setupEnabled: true,
-  fixFlash: true,
-  size: 960
-};
-</script>
-	</head>
-	<body>
+			<header>
+				<div id="topLinksFullContainer" class="container_full">
+				<div id="topLinksContainer" class="container_16">
+				
+					<ul class="socialLinksList grid_6 suffix_2">
+						
+						<li><a href="plus.google.com">Google+</a></li>
+
+						<li><a href="facebook.com">Facebook</a></li>
+						<li><a href="twitter.com">Twitter</a></li>
+						<li><a href="<?php echo site_url('rss');?>">RSS</a></li>
+					</ul>
+					<ul class="contactLinksList grid_8">
+						<li>Tel:(202)713.2526</li>
+						<li>Email: <a href="mailto:info@atomicflowtech.com?subject=feedback">Info@AtomicFlowTech.com</a></li>
+					</ul>
+				</div>
+					</div>
+				<div id="headerTopAreaFullContainer"class="container_full">
+					<div id="headerTopAreaSlider" class="container_16">
+					<h2 id="headerTopAreaSliderWords">BODY MASSAGE MACHINE BOOOY!!!!
+						<!-- <span>HTML</span>
+						<span>CSS</span>
+						<span>DESIGN</span>
+						<span>MARKETING</span> -->
+						</h2>
+					</div>
+				</div>
+				
+				
+			<div id="headerBar">
+					<div id="headerBarContent" class="container_16">
+						<a href="<?php echo base_url(); ?>"><h1 class="grid_6 suffix_1">AtomicFlow</h1></a>
+						<nav id="mainNavigation">
+							<ul id="mainNavigationList">
+								<li ><a href="<?php echo site_url('home');?>">&#43;Home</a></li>
+								<li ><a href="<?php echo site_url('about');?>">+About</a></li>
+								<li><a href="<?php echo site_url('skills');?>">+Skills</a></li>
+								<li><a href="<?php echo site_url('work');?>">+Work</a></li>
+								<li><a href="<?php echo site_url('contact');?>">+Contact</a></li>
+							</ul> <!-- end mainNavigationList -->
+						</nav> <!-- end mainNavigation -->
+					</div> <!-- end headerBarContent -->		
+				</div> <!-- end headerBar -->
+			</header>
+				
