c487c75028c82a9f2b93ffa8c125c5b1772bb6ea
Brandon John-Freso <brandonjf@atomicflowtech.com>
2011-10-24 07:46:52 -0400

    Commiting changes to the header

--- application/views/template/header_view.php
+++ application/views/template/header_view.php
@@ -15,5 +15,25 @@
 		
 		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/styles.css" media="all" />
 		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/grid/960.css" />
+		<script type="text/javascript">
+		//Script Brandon added to override default styles in the 960 Gridder overlay
+var gOverride = {
+  urlBase: 'http://gridder.andreehansson.se/releases/latest/',
+  gColor: '#EEEEEE',
+  gColumns: 16,
+  gOpacity: 0.35,
+  gWidth: 10,
+  pColor: '#C0C0C0',
+  pHeight: 15,
+  pOffset: 0,
+  pOpacity: 0.55,
+  center: true,
+  gEnabled: true,
+  pEnabled: false,//modified from true
+  setupEnabled: true,
+  fixFlash: true,
+  size: 960
+};
+</script>
 	</head>
 	<body>
--- application/views/template/nav_view.php
+++ application/views/template/nav_view.php
@@ -1,18 +1,28 @@
 			<header>
+				<div id="topLinksFullContainer" class="container_full">
 				<div id="topLinksContainer" class="container_16">
-					<ul class="socialLinksList">
-						<li><a href="plus.google.com">Google+</a></li>
+				
+					<ul class="socialLinksList grid_6 suffix_3">
+						<li ><a href="plus.google.com">Google+</a></li>
 						<li><a href="facebook.com">Facebook</a></li>
 						<li><a href="twitter.com">Twitter</a></li>
 						<li><a href="<?php echo site_url('rss');?>">RSS</a></li>
 					</ul>
-					<ul class="contactLinksList">
+					<ul class="contactLinksList grid_7">
 						<li>Tel:(202)713.2526</li>
-						<li>Email:<a href="mailto:info@atomicflowtech.com?subject=feedback">Info@AtomicFlowTech.com</a></li>
+						<li>Email: <a href="mailto:info@atomicflowtech.com?subject=feedback">Info@AtomicFlowTech.com</a></li>
 					</ul>
 				</div>
-				<div id="headerTopArea"class="container_16">
-					<h2 id="headerTopAreaGraphic" class="grid_16">HTML CSS DESIGN MARKETING</h2>
+					</div>
+				<div id="headerTopAreaFullContainer"class="container_full">
+					<div id="headerTopAreaSlider" class="container_16">
+					<h2 id="headerTopAreaSliderWords">WHY THE F*CK WONT THIS WORK!?!?
+						<!-- <span>HTML</span>
+						<span>CSS</span>
+						<span>DESIGN</span>
+						<span>MARKETING</span> -->
+						</h2>
+					</div>
 				</div> <!-- end headerTopArea -->
 				
 				<div id="headerBar">
