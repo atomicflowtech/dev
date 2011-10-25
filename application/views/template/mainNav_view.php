cdb2b466598bfda3186b651a375679f6ee11e7c5
Brandon John-Freso <brandonjf@atomicflowtech.com>
2011-10-25 00:16:41 -0400

    Commiting Nav Link IDs

--- application/views/template/mainNav_view.php
+++ application/views/template/mainNav_view.php
@@ -1,15 +1,29 @@
-			<div id="headerBar">
-					<div id="headerBarContent" class="container_16">
-						<a href="<?php echo base_url(); ?>"><h1 class="grid_6 suffix_1">AtomicFlow</h1></a>
-						<nav id="mainNavigation">
-							<ul id="mainNavigationList">
-								<li ><a href="<?php echo site_url('home');?>">&#43;Home</a></li>
-								<li ><a href="<?php echo site_url('about');?>">+About</a></li>
-								<li><a href="<?php echo site_url('skills');?>">+Skills</a></li>
-								<li><a href="<?php echo site_url('work');?>">+Work</a></li>
-								<li><a href="<?php echo site_url('contact');?>">+Contact</a></li>
-							</ul> <!-- end mainNavigationList -->
-						</nav> <!-- end mainNavigation -->
-					</div> <!-- end headerBarContent -->		
-				</div> <!-- end headerBar -->
-			</header>
+<div id="headerBar">
+	<div id="headerBarContent" class="container_16">
+		<a href="<?php echo base_url();?>"><h1 class="grid_6 suffix_1">AtomicFlow</h1></a>
+		<nav id="mainNavigation">
+			<ul id="mainNavigationList">
+				<li id="homeLink">
+					<a href="<?php echo site_url('home');?>">&#43;Home</a>
+				</li>
+				<li id="aboutLink">
+					<a href="<?php echo site_url('about');?>">+About</a>
+				</li>
+				<li id="skillsLink">
+					<a href="<?php echo site_url('skills');?>">+Skills</a>
+				</li>
+				<li id="workLink">
+					<a href="<?php echo site_url('work');?>">+Work</a>
+				</li>
+				<li id="contactLink">
+					<a href="<?php echo site_url('contact');?>">+Contact</a>
+				</li>
+			</ul>
+			<!-- end mainNavigationList -->
+		</nav>
+		<!-- end mainNavigation -->
+	</div>
+	<!-- end headerBarContent -->
+</div>
+<!-- end headerBar -->
+</header>
