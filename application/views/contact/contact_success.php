5ae5f442aff611f7a44ee5451f6c4509d6717e78
Lucas Paulger <lucas.paulger@gmail.com>
2011-11-10 18:19:50 -0500

    revert to 320dd05

---
+++ application/views/contact/contact_success.php
@@ -0,0 +1,1 @@
+<h2>Message Sent!</h2>
---
+++ application/views/template/head_view.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
+		Remove this if you use the .htaccess -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<title>{page_title}</title>
+		<meta name="description" content="{page_description}" />
+		<meta name="author" content="Brandon John-Freso and Lucas Paulger" />
+		<!-- <meta name="viewport" content="width=device-width; initial-scale=1.0" /> -->
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/fontfaces.css" media="all" />
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/grid/reset.css" media="all" />
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/default.css" media="all" />
+		
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/styles.css" media="all" />
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/grid/960.css" />
+		
+		<!-- ANYTHING SLIDER -->
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/AnythingSlider/anythingslider.css" />
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/AnythingSlider/anythingslider-ie.css" />
+		
+		<script type="text/javascript">
+		//Script Brandon added to override default styles in the 960 Gridder overlay
+var gOverride = {
+  urlBase: 'http://gridder.andreehansson.se/releases/latest/',
+  gColor: '#EEEEEE',
+  gColumns: 16,
+  gOpacity: 0.35,
+  gWidth: 10,
+  pColor: '#C0C0C0',
+  pHeight: 15,
+  pOffset: 0,
+  pOpacity: 0.55,
+  center: true,
+  gEnabled: true,
+  pEnabled: false,//modified from true
+  setupEnabled: true,
+  fixFlash: true,
+  size: 960
+};
+</script>
+	</head>
+	<body id="<?php 
+	if($this->uri->segment(1)==""){
+		echo "home";
+	}
+	else{
+		echo $this->uri->segment(1);
+	}
+	?>">
