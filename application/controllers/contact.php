14a582f418a2dd21afc745325d490a6c4ed2f904
Lucas Paulger <lucas.paulger@gmail.com>
2011-11-09 21:51:29 -0500

    created contact form with emailing backend

--- application/controllers/contact.php
+++ application/controllers/contact.php
@@ -4,6 +4,33 @@ class Contact extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('contact_view');
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+		
+		$this->form_validation->set_rules('name', 'Name', 'trim|required|xss_clean');
+		$this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+		$this->form_validation->set_rules('subject', 'Subject', 'trim|required');
+		$this->form_validation->set_rules('message', 'Message', 'required');
+		
+		$data['options'] = array('Question' => 'Question','Business proposal' => 'Business proposal','Advertisement' => 'Advertisement','Complaint' => 'Complaint');
+		
+		if ($this->form_validation->run() == FALSE)
+		{
+			$this->load->view('contact/contact_view',$data);
+		}
+		else
+		{
+			$this->load->library('email');
+
+			$this->email->from(set_value('email'), set_value('name'));
+			$this->email->to('Hello@atomicflowtech.com'); 
+			
+			$this->email->subject(set_value('subject'));
+			$this->email->message(set_value('message'));	
+			
+			$this->email->send();
+			
+			$this->load->view('contact/contact_success');
+		}
 	}
 }
