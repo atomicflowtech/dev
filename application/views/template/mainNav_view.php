9b1133e279b2a963238696fbe0d7b8c68ed2845e
Brandon John-Freso <brandonjf@atomicflowtech.com>
2011-11-13 10:35:23 -0500

    Adding some subtle changes to the nav bar and another file for the wingBar adornment

--- application/views/template/mainNav_view.php
+++ application/views/template/mainNav_view.php
@@ -1,7 +1,7 @@
 <div id="headerBar">
 	<div id="headerBarContent" class="container_16">
-		<a class="grid_7 suffix_2" href="<?php echo base_url();?>"><h1 class="grid_7">AtomicFlow</h1></a>
-		<nav id="mainNavigation" class="grid_7">
+		<a class="grid_7 suffix_2 alpha" href="<?php echo base_url();?>"><h1 class="grid_7 alpha">AtomicFlow</h1></a>
+		<nav id="mainNavigation" class="grid_7 omega">
 			<ul id="mainNavigationList" class="" >
 				<li id="homeLink" class="alpha topAreaSliderLink">
 					<a href="<?php echo site_url('home');?>">&#43;Home</a>
