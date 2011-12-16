296b33a6be2abe852bcb7fda2d0e9cbc62b9a4c8
Brandon John-Freso <brandonjf@atomicflowtech.com>
2011-12-16 03:12:12 -0500

    commiting first round of major changes to the theme

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,15 +1,11 @@
 <section>
 	<div id="mainSlider" class="container_full" >
 		<div id="mainSliderContentContainer" class="container_16">
-			<div id="mainSliderPicture" class="grid_8">
-				<img width="420px" height="343px" src="<?php echo base_url();?>images/mainSlider/imacedit.png" />
-			</div>
-			<div id="mainSliderCaptionArea" class="grid_8">
-			<div id="mainSliderCaption" >
-				<span class="captionAmpersand">&</span>
-				<p>STUNNING DESIGN <br /> INTELLIGENT CODE</p>
-			</div>
-			<div id="mainSliderSubCaption"><a href="<?php echo site_url('work');?>">Our Work</a> is the full package. </div>
+			
+				<img  src="<?php echo base_url();?>images/slides/flowfact1.png" />
+			
+			
+			
 		</div>
 		</div>
 	</div>
