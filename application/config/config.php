4f810ddf8a5dbaff1ee8d32325d2cf03ded9fb41
Brandon John-Freso <brandonjf@atomicflowtech.com>
2012-02-11 00:19:50 -0500

    Pushing current coded layout

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/AtomicFlowDev/';
+$config['base_url']	= 'http://dev.local/';
 
 /*
 |--------------------------------------------------------------------------
--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,51 +1,86 @@
+<!-- HOME_VIEW START -->
 <section>
-	<div id="mainSlider" class="container_full" >
-		<!-- <div id="mainSliderCTA" class="container_16" > -->
-		<!-- <a href="#" class="myButton">Get a Quote</a> -->
-			
-		<!-- </div> -->
-		<div id="mainSliderContentContainer" class="container_16">
-			<img  src="<?php echo base_url();?>images/slides/flowfact1.png"/>
-			<img  src="<?php echo base_url();?>images/slides/flowfact2.png"/>
-			<img  src="<?php echo base_url();?>images/slides/flowfact3.png"/>
-		</div>
-	</div>
-	</div>
-	<div id="frontBoxesBackBar" class="container_full"> </div>
-	
-			<div id="frontBoxes">
-				<div id="boxWeb" class="frontBox">
-					<header class="frontBoxHeader"><h3>Web Design</h3></header>
-						<h4 id="webSubHeader" class="frontBoxSubHeader">The Digital DaVincis</h4>
-						<p>We create interactive web experiences that sell your idea even better than you can, and might even look better doing it. Always online, Always Ready.</p>
-						<a href="<?php echo site_url('skills');?>"></a>
-				</div>
-				<div id="boxTech" class="frontBox">
-					<header class="frontBoxHeader"><h3>App Development</h3></header>
-					<!-- <h4 class="frontBoxSubHeader">The Coding Whizkids</h4> -->
-					<h4 id="techSubHeader" class="frontBoxSubHeader"></h4>
-					<p>We have creative designers and intelligent coders that make Picasso and Einstein look like under-achievers. If you can think it, we can build it.</p>
-							<a href="<?php echo site_url('skills');?>"></a>
-				</div>
-				<div id="boxBusiness" class="frontBox">
-					<header class="frontBoxHeader"><h3>Marketing</h3></header>
-					<h4 id="marketingSubHeader" class="frontBoxSubHeader">The Social Engineers</h4>
-					<p>We staff a brilliant team of highly motivated digital marketeers who eat Twitter, breathe Facebook, and Optimize Websites like it's their job.</p>
-							<a href="<?php echo site_url('skills');?>"></a>
-				</div>
-			</div>
-	<!-- <div id="subHeaderFullContainer" class="container_full">
-		<div id="subHeaderInnerContainer" class="container_16">
-			<h1 id="subHeaderTitle" class="grid_8">CREATIVE DESIGNERS. INTELLIGENT CODE.</h1>
-			<div id="taglineDescriptionContainer" class="grid_8">
-				<p>
-					AtomicFlow is an independent digital design studio based in Washington, DC.
+	<div id="mainSlider" class="container_full" ><!-- <div id="mainSliderCTA" class="container_16" >
+		--><!-- <a href="#" class="myButton">Get a Quote</a>
+	--><!-- </div>
+	-->
+	<div id="mainSliderContentContainer" class="container_16">
+		<img  src="<?php echo base_url();?>
+		images/slides/flowfact1.png"/>
+		<img  src="<?php echo base_url();?>
+		images/slides/flowfact2.png"/>
+		<img  src="<?php echo base_url();?>images/slides/flowfact3.png"/></div>
+</div>
+</div>
+<div id="frontBoxesBackBar" class="container_full"></div>
+
+<div id="frontBoxes">
+<div id="boxWeb" class="frontBox">
+	<header class="frontBoxHeader">
+		<h3>Web Design</h3>
+	</header>
+	<h4 id="webSubHeader" class="frontBoxSubHeader">The Digital DaVincis</h4>
+	<a href="<?php echo site_url('skills');?>"></a>
+</div>
+<div id="boxTech" class="frontBox">
+	<header class="frontBoxHeader">
+		<h3>App Development</h3>
+	</header>
+	<h4 id="techSubHeader" class="frontBoxSubHeader"></h4>
+
+	<a href="<?php echo site_url('skills');?>"></a>
+</div>
+<div id="boxBusiness" class="frontBox">
+	<header class="frontBoxHeader">
+		<h3>Marketing</h3>
+	</header>
+	<h4 id="marketingSubHeader" class="frontBoxSubHeader">The Social Engineers</h4>
+
+	<a href="<?php echo site_url('skills');?>"></a>
+</div>
+</div><!-- <div id="subHeaderFullContainer" class="container_full">
+<div id="subHeaderInnerContainer" class="container_16">
+<h1 id="subHeaderTitle" class="grid_8">CREATIVE DESIGNERS. INTELLIGENT CODE.</h1>
+<div id="taglineDescriptionContainer" class="grid_8">
+	<p>
+		AtomicFlow is an independent digital design studio based in Washington, DC.
 					Our clients come to us to help them create solutions through technology. We have a knack for bridging the gap between
 					your ideas and a technical reality. Our team of young motivated digital wizkids and creative geniuses will give your company
 					that extra kick it needs to get ahead in today's competitive market.
-				</p>
-			</div>
-		</div>
-	</div> -->
-	<div id="mainContentContainer" class="container_16"></div>
-</section>
+	</p>
+</div>
+</div>
+</div>
+-->
+<div id="mainContentContainerFull" class="container_full" >
+<div id="mainContentContainer" class="container_12">
+	<aside id="flowDefinitionContainer" class="grid_12">
+		 FLOW (n): THE MENTAL STATE OF OPERATION IN WHICH A PERSON IN AN ACTIVITY IS FULLY IMMERSED IN A FEELING OF ENERGIZED FOCUS, FULL INVOLVEMENT, AND SUCCESS IN THE PROCESS OF THE ACTIVITY.
+	</aside>
+
+	<div id="column1" class="grid_4">
+
+		<article class="featurette">
+			<header class="featuretteHeader">LATEST PROJECT</header>
+			<img src="/images/featurettes/DevDashFeaturette.png" alt="DevDash Screen Shot"/>
+			<h5 class="featuretteTypeHeader">Application</h4>
+			<h4 class="featuretteTitle">DevDash</h4>
+			<p class="featuretteText">Our latest application sets sights at providing development teams with an intuitive interface for communication and project managment.</p>
+		</article>
+
+	</div>
+
+
+	<div id="column2" class="grid_4">
+			<header class="featuretteHeader">OUR BLOG</header>
+			<img src="/images/featurettes/DevDashFeaturette.png" alt="DevDash Screen Shot"/>
+			<h5 class="featuretteTypeHeader">Application</h4>
+			<h4 class="featuretteTitle">DevDash</h4>
+			<p class="featuretteText">Our latest application sets sights at providing development teams with an intuitive interface for communication and project managment.</p>
+	</div>
+	<div id="column3" class="grid_4">
+			<header class="featuretteHeader">GET IN CONTACT</header>
+	</div>
+</div>
+</div>
+</section><!-- HOME_VIEW END -->
